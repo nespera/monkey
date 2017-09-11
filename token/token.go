38a1dfcb44abfec768afb355f87c64e30e3a88c8
Chris Rimmer <chris.rimmer@67bricks.com>
2017-09-11 12:59:04 +0100

    Add lexing of more keywords

--- token/token.go
+++ token/token.go
@@ -10,6 +10,11 @@ type Token struct {
 var keywords = map[string]TokenType {
 	"fn" : FUNCTION,
 	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
@@ -44,6 +49,12 @@ const (
 	LBRACE = "{"
 	RBRACE = "}"
 
+	//Keywords
 	FUNCTION = "FUNCTION"
 	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
 )
