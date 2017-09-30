ce0668d6f8d30b31a2f6129aaa5236d9e25897ad
chrisr <c@24.org.uk>
2017-09-30 21:00:39 +0100

    Added trace code

---
+++ parser/parser_tracing.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"fmt"
+	"strings"
+)
+
+var traceLevel int = 0
+
+const traceIdentPlaceholder string = "\t"
+
+func identLevel() string {
+	return strings.Repeat(traceIdentPlaceholder, traceLevel-1)
+}
+
+func tracePrint(fs string) {
+	log := fmt.Sprintf("%s%s\n", identLevel(), fs)
+	fmt.Sprint(log) //Change to Print for tracing
+}
+
+func incIdent() { traceLevel = traceLevel + 1 }
+func decIdent() { traceLevel = traceLevel - 1 }
+
+func trace(msg string) string {
+	incIdent()
+	tracePrint("BEGIN " + msg)
+	return msg
+}
+
+func untrace(msg string) {
+	tracePrint("END " + msg)
+	decIdent()
+}
